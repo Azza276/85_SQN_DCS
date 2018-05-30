9051a8a9a1b878531d634f89f05ba3de62c7837f
Azza276 <Azza276@users.noreply.github.com>
2018-05-30 21:24:12 +1000

    Create description.lua

---
+++ Liveries/f-86f sabre/85 SQN 276/description.lua
@@ -0,0 +1,125 @@
+livery = {
+
+	{"F86_MAIN", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_MAIN", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_MAIN", DECAL ,"empty",true};
+	
+	
+	
+	{"F86_BORT_NUMBER_TAIL_R_01", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_TAIL_R_01", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_TAIL_R_01", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_TAIL_R_10", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_TAIL_R_10", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_TAIL_R_10", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_TAIL_R_100", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_TAIL_R_100", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_TAIL_R_100", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_TAIL_L_01", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_TAIL_L_01", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_TAIL_L_01", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_TAIL_L_10", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_TAIL_L_10", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_TAIL_L_10", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_TAIL_L_100", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_TAIL_L_100", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_TAIL_L_100", DECAL ,"empty",true};	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	{"F86_BORT_NUMBER_R_01", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_R_01", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_R_01", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_R_10", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_R_10", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_R_10", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_R_100", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_R_100", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_R_100", DECAL ,"empty",true};	
+	
+	
+	
+	{"F86_BORT_NUMBER_L_01", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_L_01", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_L_01", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_L_10", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_L_10", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_L_10", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_L_100", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_L_100", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_L_100", DECAL ,"empty",true};
+
+
+
+
+
+
+
+
+
+
+
+	{"F86_BORT_NUMBER_NOSE_R_01", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_NOSE_R_01", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_NOSE_R_01", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_NOSE_R_10", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_NOSE_R_10", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_NOSE_R_10", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_NOSE_R_100", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_NOSE_R_100", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_NOSE_R_100", DECAL ,"empty",true};	
+	
+
+	
+	
+	{"F86_BORT_NUMBER_NOSE_L_01", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_NOSE_L_01", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_NOSE_L_01", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_NOSE_L_10", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_NOSE_L_10", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_NOSE_L_10", DECAL ,"empty",true};
+	
+	
+	{"F86_BORT_NUMBER_NOSE_L_100", 0 ,"F-86F_DIF_Azza276",false};
+	{"F86_BORT_NUMBER_NOSE_L_100", 2 ,"F-86F_SPEC_Azza276",false};
+	{"F86_BORT_NUMBER_NOSE_L_100", DECAL ,"empty",true};
+	
+	
+	
+	
+	
+	
+	
+}
+name = "85 SQN Azza276"
+countries = {"USA", "AUS",}
