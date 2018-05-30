0554d52bcac897623850c0f796f89712e8a6f63b
Azza276 <Azza276@users.noreply.github.com>
2018-05-30 21:32:38 +1000

    Create Description.lua for spitfire 85_SQN_SHY

---
+++ Liveries/SpitfireLFMkIX/85th SQN SH-Y/description.lua
@@ -0,0 +1,46 @@
+livery = {
+	{"SpitfireIX", 0 ,"SpitfireIX__85 SQN",false};
+	
+	{"NUMBER_L_0", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_L_00", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_L_000", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_L_0", DECAL ,"empty_ser",true};
+	{"NUMBER_L_00", DECAL ,"empty_ser",true};
+	{"NUMBER_L_000", DECAL ,"empty_ser",true};
+
+	{"NUMBER_L_F3_0", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_L_F3_00", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_L_F3_000", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_L_F3_0", DECAL ,"empty_ser",true};
+	{"NUMBER_L_F3_00", DECAL ,"empty_ser",true};
+	{"NUMBER_L_F3_000", DECAL ,"empty_ser",true};
+		
+	{"NUMBER_R_0", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_R_00", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_R_000", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_R_0", DECAL ,"empty_ser",true};
+	{"NUMBER_R_00", DECAL ,"empty_ser",true};
+	{"NUMBER_R_000", DECAL ,"empty_ser",true};
+	
+	{"NUMBER_R_F3_0", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_R_F3_00", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_R_F3_000", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_R_F3_0", DECAL ,"empty_ser",true};
+	{"NUMBER_R_F3_00", DECAL ,"empty_ser",true};
+	{"NUMBER_R_F3_000", DECAL ,"empty_ser",true};
+		
+	{"NUMBER_R_F3_N", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_R_F3_NN", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_L_F3_N", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_L_F3_NN", 0 ,"SpitfireIX__85 SQN",false};
+	{"NUMBER_R_F3_NN", DECAL ,"empty_ser",true};
+	{"NUMBER_R_F3_N", DECAL ,"empty_ser",true};
+	{"NUMBER_L_F3_NN", DECAL ,"empty_ser",true};
+	{"NUMBER_L_F3_N", DECAL ,"empty_ser",true};
+	
+	
+	
+	
+}
+name = "85th SQN SH-Y"
+countries = {"AUS",}
