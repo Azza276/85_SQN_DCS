75331a9268568b8cf9aef8aa347438e32e4906a2
Azza276 <Azza276@users.noreply.github.com>
2018-05-31 22:57:47 +1000

    Uploaded 85 SQN "Red Roosters" FA-18C Skin

---
+++ Liveries/f_a-18c/85th SQN Red Roosters/description.lua
@@ -0,0 +1,20 @@
+livery = {
+	{"F-18C_Texture1", 0, "F-18C_Texture1_85sqn", false},
+	{"F-18C_Texture2", 0, "F-18C_Texture2_85sqn", false},
+	{"F-18C_Texture3", 0, "F-18C_Texture3_85sqn", false},
+	{"F-18C_Texture4", 0, "F-18C_Texture4_85sqn", false},
+	{"F-18C_Texture5", 0, "F-18C_Texture5_85sqn", false},
+	{"F-18C_Texture6", 0, "F-18C_Texture6_85sqn", false},
+	
+	{"F-18C__BANO1", 0, "F-18C_Texture1_85sqn", false},
+	{"F-18C__NightCockpit", 0, "F-18C_Texture6_85sqn", false},
+	{"F-18C_Tactnumbers", 0, "empty", true},
+	{"pilot_F18C_body", 0, "pilot_fa-18c_raaf_75sqn", false},
+	{"pilot_F18C_helmet", 0, "pilot_f15_helmet", true},
+	{"F18C_Helmet_Glass", 0, "pilot_f15_helmet", true},
+
+}
+name = "85 Sqn OGN Red Roosters"
+countries = {
+	"AUS"
+}
