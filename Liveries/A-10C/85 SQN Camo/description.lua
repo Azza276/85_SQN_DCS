dbbd62ad959d1d37ac31e087f57aacbda4b229a2
Azza276 <Azza276@users.noreply.github.com>
2018-06-30 02:02:13 +1000

    Uploaded A-10C Camo Versions

---
+++ Liveries/A-10C/85 SQN Camo/description.lua
@@ -0,0 +1,24 @@
+livery = {
+	{"A-10C_PAINT_1-a", 0 ,"A-10C_85_Camo_1-a",false};
+--	{"A-10C_PAINT_1-a", 0 ,"A-10C_85_Camo_2-a",false};
+	{"A-10C_PAINT_1-b", 0 ,"A-10C_85_Camo_1-b",false};
+	{"A-10C_PAINT_1-c", 0 ,"A-10C_85_Camo_1-c",false};
+--	{"A-10C_PAINT_1-c", 0 ,"A-10C_85_Camo_2-c",false};
+	{"A-10C_PAINT_1-d", 0 ,"A-10C_85_Camo_1-d",false};
+	{"A-10C_PAINT_1-e", 0 ,"A-10C_85_Camo_1-e",false};
+	{"A-10C_PAINT_1-f", 0 ,"A-10C_85_Camo_1-f",false};
+	{"A-10C_PAINT_1-g", 0 ,"A-10C_85_Camo_1-g",false};
+	{"A-10C_PAINT_1-h", 0 ,"A-10C_85_Camo_1-h",false};
+	{"A-10C_PAINT_1-i", 0 ,"A-10C_85_Camo_1-i",false};
+	{"A-10C_PAINT_1-j", 0 ,"A-10C_85_Camo_1-j",false};
+	{"A-10C_PAINT_1-k", 0 ,"A-10C_85_Camo_1-k",false};
+	{"A-10C_PAINT_1-L", 0 ,"A-10C_85_Camo_1-l",false};
+	{"A-10_Number", 0 ,"tactnumbers-usaf-black",false};
+	{"A-10_Number_Noze_F", 0 ,"tactnumbers-usaf-black",false};
+	{"A-10_Number_Noze_T", 0 ,"empty",true};
+	{"A-10_Number_Wheel", 0 ,"tactnumbers-usaf-black",false};
+
+
+}
+name = "85 SQN A-10C Camo"
+countries = {"USA", "AUS"}
