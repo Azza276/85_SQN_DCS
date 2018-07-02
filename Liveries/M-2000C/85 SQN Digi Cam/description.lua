7dcc1f6baed5ab3d40c98f31fd035bcb56fe5faf
Azza276 <Azza276@users.noreply.github.com>
2018-07-02 22:33:10 +1000

    M2000C description

---
+++ Liveries/M-2000C/85 SQN Digi Cam/description.lua
@@ -0,0 +1,72 @@
+livery = {
+	{"ext1", 0 ,"M2KC_Digi_1T",false};
+	{"ext2", 0 ,"M2KC_Digi_2T",false};
+	{"pilot_M2KC_helmet", 0 ,"Pilot_M2KC_helmet_85",false};
+	{"pilot_M2KC", 0 ,"pilot_M2KC_85",false};
+	{"pilot_M2KC_patch", 0 ,"pilot_M2KC_patch_85",false};
+
+	{"FUSE_NUMBER1_001", 0 ,"M2KC_Digi_1T",false};
+	{"FUSE_NUMBER1_001", DECAL ,"empty",false};
+
+	{"FUSE_NUMBER2_001", 0 ,"M2KC_Digi_1T",false};
+	{"FUSE_NUMBER2_001", DECAL ,"empty",false};
+
+	{"FUSE_NUMBER2_010", 0 ,"M2KC_Digi_1T",false};
+	{"FUSE_NUMBER2_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER1_001", 0 ,"M2KC_Digi_2T",false};
+	{"TAIL_NUMBER1_001", DECAL ,"M2KC_N2_ada_ALF2",false};
+
+	{"TAIL_NUMBER1_010", 0 ,"M2KC_Digi_2T",false};
+	{"TAIL_NUMBER1_010", DECAL ,"M2KC_N2_ada_ALF2",false};
+
+	{"TAIL_NUMBER1_100", 0 ,"M2KC_Digi_2T",false};
+	{"TAIL_NUMBER1_100", DECAL ,"M2KC_N2_ada_ALF2",false};
+
+	{"TAIL_NUMBER2_001", 0 ,"M2KC_Digi_2T",false};
+	{"TAIL_NUMBER2_001", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER2_010", 0 ,"M2KC_Digi_2T",false};
+	{"TAIL_NUMBER2_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER2_100", 0 ,"M2KC_Digi_2T",false};
+	{"TAIL_NUMBER2_100", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER3_001", 0 ,"M2KC_Digi_2T",false};
+	{"TAIL_NUMBER3_001", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER3_010", 0 ,"M2KC_Digi_2T",false};
+	{"TAIL_NUMBER3_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER3_100", 0 ,"M2KC_Digi_2T",false};
+	{"TAIL_NUMBER3_100", DECAL ,"empty",false};
+
+	{"NOSE_NUMBER_001", 0 ,"M2KC_Digi_1T",false};
+	{"NOSE_NUMBER_001", DECAL ,"M2KC_N2_ada_ALF2",false};
+
+	{"NOSE_NUMBER_010", 0 ,"M2KC_Digi_1T",false};
+	{"NOSE_NUMBER_010", DECAL ,"M2KC_N2_ada_ALF2",false};
+
+	{"NOSE_NUMBER_100", 0 ,"M2KC_Digi_1T",false};
+	{"NOSE_NUMBER_100", DECAL ,"M2KC_N2_ada_ALF2",false};
+
+	{"WINGR_NUMBER_001", 0 ,"M2KC_Digi_2T",false};
+	{"WINGR_NUMBER_001", DECAL ,"M2KC_N2_ada_ALF2",false};
+
+	{"WINGR_NUMBER_010", 0 ,"M2KC_Digi_2T",false};
+	{"WINGR_NUMBER_010", DECAL ,"M2KC_N2_ada_ALF2",false};
+
+	{"WINGR_NUMBER_100", 0 ,"M2KC_Digi_2T",false};
+	{"WINGR_NUMBER_100", DECAL ,"M2KC_N2_ada_ALF2",false};
+
+	{"WINGL_NUMBER_001", 0 ,"M2KC_Digi_1T",false};
+	{"WINGL_NUMBER_001", DECAL ,"empty",false};
+
+	{"WINGL_NUMBER_010", 0 ,"M2KC_Digi_1T",false};
+	{"WINGL_NUMBER_010", DECAL ,"empty",false};
+
+	{"WINGL_NUMBER_100", 0 ,"M2KC_Digi_1T",false};
+	{"WINGL_NUMBER_100", DECAL ,"empty",false};
+}
+name = "85 SQN Digital Camo"
+countries = {"FRA", "AUS"}
