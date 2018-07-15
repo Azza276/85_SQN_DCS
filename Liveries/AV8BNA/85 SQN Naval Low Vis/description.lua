b48294adc5b26216bd4a71981485068841a4743a
Azza276 <Azza276@users.noreply.github.com>
2018-07-15 23:51:37 +1000

    Upload AV-8B Naval Low Vis

---
+++ Liveries/AV8BNA/85 SQN Naval Low Vis/description.lua
@@ -0,0 +1,46 @@
+livery = {
+
+	{"AV8B1", 0 ,"AV8_T1_85",false};
+	{"AV8B2", 0 ,"AV8_T2_85",false};
+	{"AV8B3", 0 ,"AV8_T3_85",false};
+	
+	{"AV8B_T1_FUSE1", 0 ,"AV8_T1_85",false};
+	{"AV8B_T1_FUSE1", DECAL ,"AV8_TNumbers",false};
+	
+	{"AV8B_T1_FUSE10", 0 ,"AV8_T1_85",false};
+	{"AV8B_T1_FUSE10", DECAL ,"AV8_TNumbers",false};
+	
+	{"AV8B_T2_FUSE1", 0 ,"AV8_T2_85",false};
+	{"AV8B_T2_FUSE1", DECAL ,"AV8_TNumbers",false};
+	
+	{"AV8B_T2_FUSE10", 0 ,"AV8_T2_85",false};
+	{"AV8B_T2_FUSE10", DECAL ,"AV8_TNumbers",false};
+	
+	{"AV8B_T3_FUSE1", 0 ,"AV8_T3_85",false};
+	{"AV8B_T3_FUSE1", DECAL ,"AV8_TNumbers",false};
+	
+	{"AV8B_T3_FUSE10", 0 ,"AV8_T3_85",false};
+	{"AV8B_T3_FUSE10", DECAL ,"AV8_TNumbers",false};
+	
+	{"AV8B_T3_TAIL1", 0 ,"AV8_T3_85",false};
+	{"AV8B_T3_TAIL1", DECAL ,"AV8_TNumbers",false};
+	
+	{"AV8B_T3_TAIL10", 0 ,"AV8_T3_85",false};
+	{"AV8B_T3_TAIL10", DECAL ,"AV8_TNumbers",false};
+	
+	{"AV8B_T3_TAIL100", 0 ,"AV8_T3_85",false};
+	{"AV8B_T3_TAIL100", DECAL ,"AV8_TNumbers",false};
+	
+	{"pilot_AV8B_patch", 0 ,"empty",true};
+	{"AV8B1", ROUGHNESS_METALLIC ,"AV8_T1_RoughMet",true};
+	{"AV8B2", ROUGHNESS_METALLIC ,"AV8_T2_RoughMet",true};
+	{"AV8B3", ROUGHNESS_METALLIC ,"AV8_T3_RoughMet",true};
+	{"AV8B4", ROUGHNESS_METALLIC ,"AV8_T4_RoughMet",true};
+	{"AV8B7", ROUGHNESS_METALLIC ,"AV8_T7_RoughMet",true};
+	{"AV8B8", ROUGHNESS_METALLIC ,"AV8_T8_RoughMet",true};
+
+}
+
+name = "85 SQN Naval Low Vis"
+
+countries = {"AUS", "USA"}
