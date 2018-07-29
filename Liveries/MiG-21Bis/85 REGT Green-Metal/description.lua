b645b85a895942d2439c95185dceb1e90a1357be
Azza276 <Azza276@users.noreply.github.com>
2018-07-29 23:57:01 +1000

    Create description.lua

---
+++ Liveries/MiG-21Bis/85 REGT Green-Metal/description.lua
@@ -0,0 +1,112 @@
+
+-- r +45, g 0, b +40  Legacy Contrast
+
+livery = {
+
+-- Merged Textures
+{"MIG21BIS_01", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_01", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_01", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_01", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+                                     
+{"MIG21BIS_02", 0 ,"MiG21Bis_85_02_Dif",false};
+{"MIG21BIS_02", 1 ,"MiG21Bis_02_NMp",true};
+{"MIG21BIS_02", 2 ,"MiG21Bis_85_02_Spec",false};
+{"MIG21BIS_02", ROUGHNESS_METALLIC ,"MiG21Bis_02_Dif_RoughMet",false};
+                
+
+				
+-- Tac Number Nose
+{"MIG21BIS_SER_NOSE_0001", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_SER_NOSE_0001", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_SER_NOSE_0001", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_SER_NOSE_0001", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+{"MIG21BIS_SER_NOSE_0001", DECAL ,"MiG21Bis_Ser_Germany-East-Red-Stencil",true};
+--{"MIG21BIS_SER_NOSE_0001", DECAL ,"MiG21Bis_Ser_Fuse",true};
+
+{"MIG21BIS_SER_NOSE_0010", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_SER_NOSE_0010", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_SER_NOSE_0010", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_SER_NOSE_0010", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+{"MIG21BIS_SER_NOSE_0010", DECAL ,"MiG21Bis_Ser_Germany-East-Red-Stencil",true};
+--{"MIG21BIS_SER_NOSE_0010", DECAL ,"MiG21Bis_Ser_Fuse",true};
+
+{"MIG21BIS_SER_NOSE_0100", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_SER_NOSE_0100", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_SER_NOSE_0100", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_SER_NOSE_0100", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+{"MIG21BIS_SER_NOSE_0100", DECAL ,"MiG21Bis_Ser_Germany-East-Red-Stencil",true};
+--{"MIG21BIS_SER_NOSE_0100", DECAL ,"MiG21Bis_Ser_Fuse",true};
+
+{"MIG21BIS_SER_NOSE_1000", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_SER_NOSE_1000", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_SER_NOSE_1000", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_SER_NOSE_1000", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+{"MIG21BIS_SER_NOSE_1000", DECAL ,"empty_ser",true};
+--{"MIG21BIS_SER_NOSE_1000", DECAL ,"MiG21Bis_Ser_Fuse",true};
+
+
+-- Tac Number Fuselage
+{"MIG21BIS_SER_FUSE_001", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_SER_FUSE_001", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_SER_FUSE_001", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_SER_FUSE_001", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+{"MIG21BIS_SER_FUSE_001", DECAL ,"empty_ser",true};
+--{"MIG21BIS_SER_FUSE_001", DECAL ,"MiG21Bis_Ser_Fuse",true};
+
+{"MIG21BIS_SER_FUSE_010", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_SER_FUSE_010", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_SER_FUSE_010", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_SER_FUSE_010", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+{"MIG21BIS_SER_FUSE_010", DECAL ,"empty_ser",true};
+--{"MIG21BIS_SER_FUSE_010", DECAL ,"MiG21Bis_Ser_Fuse",true};
+
+{"MIG21BIS_SER_FUSE_100", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_SER_FUSE_100", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_SER_FUSE_100", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_SER_FUSE_100", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+{"MIG21BIS_SER_FUSE_100", DECAL ,"empty_ser",true};
+--{"MIG21BIS_SER_FUSE_100", DECAL ,"MiG21Bis_Ser_Fuse",true};
+
+
+-- Tac Number Tail
+{"MIG21BIS_SER_TAIL_000001", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_SER_TAIL_000001", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_SER_TAIL_000001", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_SER_TAIL_000001", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+{"MIG21BIS_SER_TAIL_000001", DECAL ,"empty_ser",true};
+--{"MIG21BIS_SER_TAIL_000001", DECAL ,"MiG21Bis_Ser_Tail",true};
+
+{"MIG21BIS_SER_TAIL_000010", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_SER_TAIL_000010", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_SER_TAIL_000010", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_SER_TAIL_000010", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+{"MIG21BIS_SER_TAIL_000010", DECAL ,"empty_ser",true};
+--{"MIG21BIS_SER_TAIL_000010", DECAL ,"MiG21Bis_Ser_Tail",true};
+
+{"MIG21BIS_SER_TAIL_000100", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_SER_TAIL_000100", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_SER_TAIL_000100", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_SER_TAIL_000100", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+{"MIG21BIS_SER_TAIL_000100", DECAL ,"empty_ser",true};
+--{"MIG21BIS_SER_TAIL_000100", DECAL ,"MiG21Bis_Ser_Tail",true};
+
+{"MIG21BIS_SER_TAIL_001000", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_SER_TAIL_001000", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_SER_TAIL_001000", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_SER_TAIL_001000", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+{"MIG21BIS_SER_TAIL_001000", DECAL ,"empty_ser",true};
+--{"MIG21BIS_SER_TAIL_001000", DECAL ,"MiG21Bis_Ser_Tail",true};
+
+{"MIG21BIS_SER_TAIL_010000", 0 ,"MiG21Bis_85_01_Dif",false};
+{"MIG21BIS_SER_TAIL_010000", 1 ,"MiG21Bis_01_NMp",true};
+{"MIG21BIS_SER_TAIL_010000", 2 ,"MiG21Bis_85_01_Spec",false};
+{"MIG21BIS_SER_TAIL_010000", ROUGHNESS_METALLIC ,"MiG21Bis_01_Dif_RoughMet",false};
+{"MIG21BIS_SER_TAIL_010000", DECAL ,"empty_ser",true};
+--{"MIG21BIS_SER_TAIL_010000", DECAL ,"MiG21Bis_Ser_Tail",true};
+
+}
+
+name = "85 Regt Green|Metal"
+
+--countries = {"USA", "RUS", "AUS", "UKR", "SPN", "NETH", "TUR", "BEL", "GER", "NOR", "CAN", "DEN", "UK", "GRG", "ISR", "ABH", "RSO"}
